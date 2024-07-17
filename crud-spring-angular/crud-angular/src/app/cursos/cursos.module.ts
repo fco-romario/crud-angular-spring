3223cc9eb1160583f6c6b333b90ef01d5e34b735
fco-romario <fco.romario.alves@gmail.com>
2024-07-17 20:46:46 -0300

    criando componente de formulario e criando o seu roteamento

--- crud-spring-angular/crud-angular/src/app/cursos/cursos.module.ts
+++ crud-spring-angular/crud-angular/src/app/cursos/cursos.module.ts
@@ -5,11 +5,13 @@ import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosComponent } from './cursos/cursos.component';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { CursoFormComponent } from './curso-form/curso-form.component';
 
 
 @NgModule({
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursoFormComponent
   ],
   imports: [
     CommonModule,
--- crud-spring-angular/crud-angular/src/app/cursos/cursos/cursos.component.ts
+++ crud-spring-angular/crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -1,6 +1,7 @@
 import { CursosService } from './../services/cursos.service';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, catchError, of } from 'rxjs';
 import { Curso } from 'src/app/cursos/model/curso';
 import { ErrorDialogComponent } from 'src/app/shared/componentes/error-dialog/error-dialog.component';
@@ -12,11 +13,13 @@ import { ErrorDialogComponent } from 'src/app/shared/componentes/error-dialog/er
 })
 export class CursosComponent implements OnInit {
   cursos$: Observable<Curso[]>;
-  displayedColumns = ['_id', 'name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private cursosService: CursosService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
   ) {
     this.cursos$ = this.cursosService.list().pipe(
       catchError(error => {
@@ -36,6 +39,10 @@ export class CursosComponent implements OnInit {
       data: errorMsg
     }
     )
+  }
+
+  onAdd(): void {
+    this.router.navigate(['new'], { relativeTo: this.route})
   }
 
 }
