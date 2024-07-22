5e3fb305aaddd715b34573c1643ff09d5b622acb
fco-romario <fco.romario.alves@gmail.com>
2024-07-22 19:30:38 -0300

    buscar por id back-end

--- crud-spring-angular/crud-spring/src/main/java/crud/romario/controller/CursoController.java
+++ crud-spring-angular/crud-spring/src/main/java/crud/romario/controller/CursoController.java
@@ -3,7 +3,9 @@ package crud.romario.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,13 @@ public class CursoController {
 	@GetMapping()
 	public List<Curso> listar() {
 		return cursoRepository.findAll();
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Curso> findById(@PathVariable Long id) {
+		return cursoRepository.findById(id)
+				.map(curso -> ResponseEntity.ok().body(curso))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping()
