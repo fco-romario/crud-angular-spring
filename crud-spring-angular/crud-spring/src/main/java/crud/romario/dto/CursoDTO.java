312c0017307dbc5835461a27545b954f27d7567a
fco-romario <fco.romario.alves@gmail.com>
2024-08-16 21:26:57 -0300

    API: Validando enums com java bean validations

--- crud-spring-angular/crud-spring/src/main/java/crud/romario/dto/CursoDTO.java
+++ crud-spring-angular/crud-spring/src/main/java/crud/romario/dto/CursoDTO.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import crud.romario.enums.Category;
+import crud.romario.enums.validation.ValueOfEnum;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -15,6 +17,6 @@ import jakarta.validation.constraints.Size;
 public record CursoDTO(
 		@JsonProperty("_id") Long id,
 		@NotBlank @NotNull @Size(min = 5, max = 100) String name,
-		@NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
-		@NotBlank @NotEmpty @Valid List<AulaDTO> aulas) {
+		@NotNull @Size(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
+		@NotNull @NotEmpty @Valid List<AulaDTO> aulas) {
 }
---
+++ crud-spring-angular/crud-spring/src/main/java/crud/romario/enums/validation/ValueOfEnum.java
@@ -0,0 +1,28 @@
+package crud.romario.enums.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+public @interface ValueOfEnum {
+    Class<? extends Enum<?>> enumClass();
+    String message() default "must be any of enum {enumClass}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
